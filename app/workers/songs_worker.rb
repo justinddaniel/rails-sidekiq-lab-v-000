b73711b57b61ae82277646b13eae61e92de3fd89
Justin Daniel <justinddaniel@gmail.com>
2018-02-05 23:53:19 +0000

    automatically backed up by learn

--- app/workers/songs_worker.rb
+++ app/workers/songs_worker.rb
@@ -4,7 +4,7 @@ class LeadsWorker
 
   def perform(file)
     CSV.foreach(file, headers: true) do |song|
-    Song.create(title: song[0], artist_name: song[1])
+      Song.create(title: song[0], artist_name: song[1])
     end
   end
 end
