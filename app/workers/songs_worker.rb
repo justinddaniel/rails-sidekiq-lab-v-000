820b61a1bf942d0a4d7c48fdd8e88d7fb329604d
Justin Daniel <justinddaniel@gmail.com>
2018-02-05 23:52:47 +0000

    automatically backed up by learn

--- app/workers/songs_worker.rb
+++ app/workers/songs_worker.rb
@@ -1,9 +1,10 @@
 class LeadsWorker
+  require 'csv'
   include Sidekiq::Worker
 
   def perform(file)
     CSV.foreach(params["file"].path, headers: true) do |song|
     Song.create(title: song[0], artist_name: song[1])
-  end
+    end
   end
 end
